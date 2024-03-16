08b9391613afd9a1e2260736a7cb20fa2eb79826
Amirul Azreen <143872030+amirulazreen@users.noreply.github.com>
2024-03-16 13:13:05 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "",
+      redirect: { name: "inventory" },
+    },
+    {
       path: "/inventory",
       name: "inventory",
       component: InventoryView,
